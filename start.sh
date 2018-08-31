c94a7605f3c552fb88fd25a5d58febd00f14d4e8
spdir <aery_mzc9123@163.com>
2018-08-31 09:32:53 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 _NOW_PATH=`pwd`
-python3 ${_NOW_PATH}/main.py --logging=none  &> /dev/null &
+python3 ${_NOW_PATH}/main.py --logging=none
 if [[ $? != 0 ]]; then
-  python ${_NOW_PATH}/main.py --logging=none &> /dev/null &
+  python ${_NOW_PATH}/main.py --logging=none
 fi
