e425e072686cad9e0f29008ba70314284a1a82a3
Musker <aery_mzc9123@163.com>
2018-09-01 08:44:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-WHOIS_DIR=/opt/whois-query/main.py
+WHOIS_DIR=`pwd`/whois-query/main.py
 python3 ${WHOIS_DIR} --logging=none
 if [[ $? != 0 ]]; then
   python ${WHOIS_DIR} --logging=none
